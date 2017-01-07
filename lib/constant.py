16c925ca0f9f95af591d9f010d24f57c07cc648d
ssteo <teo@binary.com>
2017-01-07 15:39:15 +0800

    initial commit

---
+++ lib/constant.py
@@ -0,0 +1,112 @@
+# Use machine_type as key, (npe_pricing, pe_pricing) as value
+GCE_PRICING_TABLE = {}
+GCE_PRICING_TABLE['f1-micro']       = (0.006, 0.005)
+GCE_PRICING_TABLE['g1-small']       = (0.021, 0.010)
+GCE_PRICING_TABLE['n1-standard-1']  = (0.038, 0.015)
+GCE_PRICING_TABLE['n1-standard-2']  = (0.076, 0.030)
+GCE_PRICING_TABLE['n1-standard-4']  = (0.152, 0.060)
+GCE_PRICING_TABLE['n1-standard-8']  = (0.304, 0.120)
+GCE_PRICING_TABLE['n1-standard-16'] = (0.608, 0.240)
+GCE_PRICING_TABLE['n1-standard-32'] = (1.216, 0.480)
+GCE_PRICING_TABLE['n1-highmem-2']   = (0.096, 0.035)
+GCE_PRICING_TABLE['n1-highmem-4']   = (0.192, 0.070)
+GCE_PRICING_TABLE['n1-highmem-8']   = (0.384, 0.140)
+GCE_PRICING_TABLE['n1-highmem-16']  = (0.768, 0.280)
+GCE_PRICING_TABLE['n1-highmem-32']  = (1.536, 0.560)
+GCE_PRICING_TABLE['n1-highcpu-2']   = (0.058, 0.020)
+GCE_PRICING_TABLE['n1-highcpu-4']   = (0.116, 0.040)
+GCE_PRICING_TABLE['n1-highcpu-8']   = (0.232, 0.080)
+GCE_PRICING_TABLE['n1-highcpu-16']  = (0.464, 0.160)
+GCE_PRICING_TABLE['n1-highcpu-32']  = (0.928, 0.320)
+
+API_FAILURE_MESSAGE     = '%s() failed: %s'
+API_MAX_RETRY_NESSAGE   = '%s() max. retry exceeded after %s call(s). Reason: %s'
+API_RETRY_MESSAGE       = '%s() failed: %s. Retrying...'
+API_POLLING_INTERVAL    = 2
+API_TYPE                = 'compute'
+API_VERSION             = 'v1'
+
+GCE_PREEMPTIBLE         = 'PE'
+GCE_NON_PREEMPTIBLE     = 'NPE'
+
+GCE_STATUS_PROVISIONING = 'PROVISIONING'
+GCE_STATUS_STAGING      = 'STAGING'
+GCE_STATUS_RUNNING      = 'RUNNING'
+GCE_STATUS_STOPPING     = 'STOPPING'
+GCE_STATUS_TERMINATED   = 'TERMINATED'
+
+INSTANCE_FLAG_NEW       = 'NEW'
+INSTANCE_FLAG_MATURED   = 'MATURED'
+INSTANCE_FLAG_RECYCLED  = 'RECYCLED'
+
+INDEX_INSTANCE_COUNT    = 0
+INDEX_ZONE_NAME         = 1
+INDEX_TERMINATION_RATE  = 2
+INDEX_UPTIME_HOUR       = 3
+
+ALL_ZONE                = True
+PE_AVAILABLE_ZONE_ONLY  = False
+PREEMPTIBLE             = True
+NON_PREEMPTIBLE         = False
+
+INSTANCE_MATURITY_HOUR  = 23
+LOGGER_MAX_LINE_BUFFER  = 500
+MAX_API_RETRY_COUNT     = 2
+PRETTY_PRINT_INDENT     = 4
+PRETTY_PRINT_WIDTH      = 80
+HOUR_PER_SECOND         = (float(1) / 3600)
+REPORT_LOG_COUNT        = 10
+UPTIME_DECIMAL          = 5
+
+CURRENT_PROCESS         = '/proc/self'
+DEFAULT_EMAIL_FOOTER    = 'For more details, go to https://github.com/binary-com/gce-manager'
+DEFAULT_EMAIL_SUBJECT   = 'Report of GCE instance changes'
+DEFAULT_EMAIL_TAG       = '[GCE-MANAGER]'
+DEFAULT_LOGGER_NAME     = 'gce_manager'
+ERROR_THREAD_CRASHED    = 'Thread is crashed in GCE-MANAGER!'
+HTML_LINE_BREAK_TAG     = '<br />'
+
+MESSAGE_CREATED         = '%s:%s@%s is created'
+MESSAGE_DELETED         = '%s:%s@%s is deleted after %s hour(s)'
+MESSAGE_RUNNING         = '%s:%s@%s is running for %s hour(s)'
+MESSAGE_STARTED         = '%s:%s@%s is online'
+MESSAGE_TERMINATED      = '%s:%s@%s is terminated after %s hour(s)'
+
+MESSAGE_CONVERT_PE      = 'Converting %s:%s@%s to preemptible instance after %s hour(s)'
+MESSAGE_CONVERT_NPE     = 'Converting %s:%s@%s to non-preemptible instance after %s hour(s)'
+MESSAGE_RECYCLE         = 'Recycling %s:%s@%s after %s hour(s)'
+MESSAGE_RELOCATE        = 'Relocating %s:%s@%s to a different zone after %s hour(s)'
+MESSAGE_PE_HIGH_DEMAND  = 'Exceeded threshold of total zone(s) with high demand in preemptible instance'
+MESSAGE_SAME_ZONE       = 'Destination zone candidate for relocation is same as current zone'
+
+LOG_RECORD_FORMAT       = '[%(asctime)s] %(levelname)s - %(message)s'
+LOG_TIMESTAMP_FORMAT    = '%Y-%m-%d %H:%M:%S'
+PICKLE_FILE_EXTENSION   = '.pkl'
+PICKLE_FILE_PATH_FORMAT = '%s/.%s%s'
+
+REPORT_TEMPLATE         = '%s##Estimated Cost/Savings#%s##Zone(s) Configured#%s##Instance List#%s##GCE Manager Configuration#%s##%s'.replace('#', HTML_LINE_BREAK_TAG)
+SHUTDOWN_MESSAGE        = 'Received SIGHUP signal for graceful shutdown. Exiting...'
+STARTUP_MESSAGE         = 'Instance monitoring started for project \'%s\''
+USAGE_MESSAGE           = 'Usage: gce_manager.py <config_file.yml>'
+
+SLACKBOT_CMD_HELP       = 'help'
+SLACKBOT_CMD_CONFIG     = 'show config'
+SLACKBOT_CMD_LI         = 'show instance list'
+SLACKBOT_CMD_LZ         = 'show zone list'
+SLACKBOT_CMD_SAVINGS    = 'show savings'
+
+SLACKBOT_EMOJI          = ':snowman:'
+SLACKBOT_ERR_CONNECT    = 'Connection to Slack failed, invalid token?'
+SLACKBOT_MSG_ACK        = '@%s Here\'s the information that you\'ve requested\n'
+SLACKBOT_MSG_COST_NOTE  = 'Note: Savings are calculated based on total of (PE-hour x NPE-cost/hour) - (PE-hour x PE-cost/hour)'
+SLACKBOT_MSG_HELP       = '```Commands available:\n1. show config\n2. show instance list\n3. show savings\n4. show zone list\n5. help\n\n%s```' % DEFAULT_EMAIL_FOOTER
+SLACKBOT_MSG_UNAUTH     = 'Nice try @%s but I\'m not authorized to serve your request :hand:'
+SLACKBOT_MSG_UNKNOWN    = 'I\'m sorry @%s. I don\'t understand your request. Type @%s help to see available commands'
+SLACKBOT_USERNAME       = 'gcebot'
+
+TABLE_TITLE_COST        = ['Usage Type', 'Usage Hour', 'Cost/Hour', 'Total', 'Savings']
+TABLE_TITLE_INSTANCE    = ['Node', 'Zone', 'Private IP', 'Type', 'Uptime Hour', 'Flag', 'Status']
+TABLE_TITLE_ZONE        = ['Zone', 'Instance', 'Uptime Hour', 'Termination', 'Termination Rate']
+
+ERR_INSTANCE_LESSER_THAN_ZONE_SPREAD    = 'Minimum instance count must be greater or equal to the minimum number of zone(s) to be spread evenly'
+ERR_ZONES_LESSER_THAN_ZONE_SPREAD       = 'Minimum zone count must be greater or equal to the minimum number of zone(s) to be spread evenly'
---
+++ lib/slackbot.py
@@ -0,0 +1,139 @@
+import signal
+import sys
+import time
+
+from config import *
+from constant import *
+from datetime import datetime
+from slackclient import SlackClient
+from util import *
+
+class Slackbot:
+    def __init__(self, config_obj):
+        self.abort_all = False
+        self.bot_tag = None
+        self.config = config_obj
+        self.util = Util('slackbot')
+        self.logger = self.util.logger
+        self.sc = SlackClient(self.config.SLACKBOT_API_TOKEN)
+        self._msg_queue, self.config_table, self.cost_table, self.instance_table, self.zone_table = [], [], [], [], []
+
+    def format_slack_table(self, table, note=None, make_single_column=False):
+        single_column_table = ''
+
+        for row in table:
+            _row, first_record = '', True
+            for record in row:
+                if first_record:
+                    _row += '*%s*' % record + '\n'
+                    first_record = False
+                else:
+                    _row += '>%s' % record + '\n'
+            single_column_table += _row + '\n'
+
+        note_str = note if note != None else ''
+        multi_column_table = '''```%s\n%s```''' % (self.util.get_ascii_table(table), note_str)
+        single_column_table = single_column_table if note == None else '%s\n%s' % (single_column_table, note)
+        return single_column_table if make_single_column else multi_column_table
+
+    def get_channel_name(self, channel):
+        if channel != None:
+            channel_info = self.sc.api_call('channels.info', channel=channel)
+            channel_name = channel_info.get('channel')['name'] if channel_info.get('channel') != None else None
+            return '#%s' % channel_name if channel_name != None else channel
+        else:
+            return None
+
+    def get_current_user_info(self):
+        return self.sc.api_call('auth.test', token=self.config.SLACKBOT_API_TOKEN)
+
+    def get_message(self, payload):
+        user_name = self.get_user_name(payload.get('user'))
+        channel_name = self.get_channel_name(payload.get('channel'))
+        timestamp = self.get_timestamp(float(payload.get('ts') if payload.get('ts') != None else 0))
+        text = payload.get('text')
+        valid_message = channel_name != None and text != None and user_name != None
+        return (channel_name, text, timestamp, user_name) if valid_message else None
+
+    def get_timestamp(self, timestamp):
+        if timestamp > 0:
+            return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S.%f')
+        else:
+            return None
+
+    def get_user_name(self, user_id):
+        if user_id != None:
+            user_info = self.sc.api_call("users.info", user=user_id)
+            return user_info.get('user')['name'] if user_info.get('user') != None else user_id
+        else:
+            return None
+
+    def process_command(self, channel_name, text, timestamp, caller_name):
+        header, lowercase_text, message = (SLACKBOT_MSG_ACK % caller_name), text.lower(), ''
+
+        if SLACKBOT_CMD_HELP in lowercase_text:
+            message = SLACKBOT_MSG_HELP
+        elif SLACKBOT_CMD_CONFIG in lowercase_text:
+            message = self.format_slack_table(self.config_table, None, True)
+        elif SLACKBOT_CMD_LI in lowercase_text:
+            message = self.format_slack_table(self.instance_table)
+        elif SLACKBOT_CMD_SAVINGS in lowercase_text:
+            message = self.format_slack_table(self.cost_table, note=SLACKBOT_MSG_COST_NOTE)
+        elif SLACKBOT_CMD_LZ in lowercase_text:
+            message = self.format_slack_table(self.zone_table)
+        else:
+            header, message = '', SLACKBOT_MSG_UNKNOWN % (caller_name, SLACKBOT_USERNAME)
+
+        self.send_message(channel_name, '%s%s' % (header, message))
+
+    def send_message(self, channel, message, username=SLACKBOT_USERNAME, icon_emoji=SLACKBOT_EMOJI):
+        self._msg_queue.append((channel, message, username, icon_emoji))
+
+    def start_bot(self):
+        if self.sc.rtm_connect():
+            try:
+                self.bot_tag = '<@%s>' % self.get_current_user_info().get('user_id')
+                self.channel_message_monitor()
+            except Exception, exception:
+                self.logger.info(API_FAILURE_MESSAGE % (sys._getframe().f_code.co_name, exception))
+        else:
+            self.logger.info(SLACKBOT_ERR_CONNECT)
+
+    def channel_message_monitor(self):
+        while not self.abort_all:
+            buffer = self.sc.rtm_read()
+
+            if len(buffer) > 0 and buffer[0]['type'] == 'message':
+                message = self.get_message(buffer[0])
+
+                if message != None:
+                    channel_name, text, timestamp, user_name = message
+
+                    if self.bot_tag not in text:
+                        continue
+
+                    if user_name in self.config.SLACKBOT_USER_LIST:
+                        self.process_command(channel_name, text, timestamp, user_name)
+                    else:
+                        self.send_message(channel_name, SLACKBOT_MSG_UNAUTH % user_name)
+
+            # Sleep 1 second for each API call for avoid hitting Slack API rate limit
+            time.sleep(1)
+
+            if len(self._msg_queue) > 0:
+                channel, message, username, icon_emoji = self._msg_queue.pop(0)
+                self.sc.api_call("chat.postMessage", channel=channel, text=message, username=username, icon_emoji=icon_emoji)
+
+    def shutdown(self, message=None):
+        if not self.abort_all:
+            if message is not None:
+                self.logger.info(message)
+            self.abort_all = True
+
+if __name__ == "__main__":
+    try:
+        # Standalone mode for development/testing purposes
+        slackbot = Slackbot(Config('../config.yml'))
+        slackbot.start_bot()
+    except KeyboardInterrupt:
+        slackbot.shutdown('Exiting...')
